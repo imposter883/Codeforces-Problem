4d7b560e680ad446fee6ce2dc85e22c5be2619d9
Abdullah Shoab <imshoab883@gmail.com>
2024-12-01 20:18:50 +0600

    Add files via upload

---
+++ 1850B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int winner = -1;
+        int best_score = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int a, b;
+            cin >> a >> b;
+
+            if (b > best_score && a <= 10) {
+                winner = i;
+                best_score = b;
+            }
+        }
+
+        cout << winner << endl;
+    }
+
+    return 0;
+}
